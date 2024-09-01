019068488a920f1bfd636e1c0236470667e1ca97
许浩峰 <2731897659@qq.com>
2024-09-01 23:35:58 +0800

    after javaScript

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,26 @@ myImage.onclick = function (){
         myImage.setAttribute("src", "../pictures/img1.jpg");
     }
 }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+if(localStorage.getItem("name") === null){
+    setUserName();
+}
+else{
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Welcome to the website, " + storedName;
+}
+function setUserName() {
+    let myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name",myName);
+        myHeading.textContent = "Welcome to the website, " + myName;
+    }
+}
+myButton.onclick = function(){
+    setUserName();
+}
