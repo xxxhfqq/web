ebfaa1689c8ffaa64bd4e104eaa423433758715c
许浩峰 <2731897659@qq.com>
2024-09-01 17:12:21 +0800

    1

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello javaScript!";
+let myImage = document.querySelector("img");
+
+myImage.onclick = function (){
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "../pictures/img1.jpg") {
+        myImage.setAttribute("src", "../pictures/img2.jpg");
+    } else {
+        myImage.setAttribute("src", "../pictures/img1.jpg");
+    }
+}
